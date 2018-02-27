1867164bde49856ae37b3a335b7c29e4e01fd4ec
i6monitor <developer@i6monitor.com>
2018-02-27 22:17:19 +0700

    Update API

--- assets/static/js/1.1f47f2c5a74c4267a996.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{647:function(l,e,u){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var n=u(1),t=n(null,null,null,null,null);e.default=t.exports}});
-//# sourceMappingURL=1.1f47f2c5a74c4267a996.js.map
